be7c204dbbc01f1321ebca80447789fc0a1ef07a
Anton <79142916+skyfxllen@users.noreply.github.com>
2025-03-19 19:30:23 +0700

    styles for link

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -7,12 +7,16 @@ const NotFound = () : JSX.Element => (
     }}
   >
     <h1>404 Not Found</h1>
-    <span style={{
-      color: 'blue',
-      textDecoration: 'underline'
-    }}
-    >
-      <Link to={'/'} >Main Page</Link>
+    <span>
+      <Link
+        to={'/'}
+        style={{
+          color: 'blue',
+          textDecoration: 'underline'
+        }}
+      >
+        Main Page
+      </Link>
     </span>
   </div>
 );
