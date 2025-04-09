8c29be3ff0bf2684d4012d4f7db3a6277e4f5ec5
Anton <79142916+skyfxllen@users.noreply.github.com>
2025-04-09 20:27:59 +0700

    module7-task1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,57 +6,39 @@ import { Offer } from '../../pages/Offer/Offer';
 import { NotFound } from '../../pages/NotFound/NotFound';
 import { PrivateRoute } from '../PrivateRoute/PrivateRoute';
 import { AppRoute } from '../../const';
-import { OfferType } from '../../types/offers';
-import { FavoritesByCity } from '../../types/favorites';
 import { Review } from '../../types/review';
-import { mockOffers } from '../../mocks/offers';
 type AppProps = {
   reviews: Review[];
 }
 
-const App = ({ reviews } : AppProps) : JSX.Element => {
-  function getFavoritesByCity(offersList: OfferType[]) {
-    return offersList
-      .filter((offer) => offer.isFavorite)
-      .reduce((acc: FavoritesByCity, offer) => {
-        const cityName = offer.city.name;
-        if (!acc[cityName]) {
-          acc[cityName] = [];
+const App = ({ reviews } : AppProps) : JSX.Element => (
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path={AppRoute.Root}
+        element={<Main />}
+      />
+      <Route
+        path={AppRoute.Login}
+        element={<Login />}
+      />
+      <Route
+        path={AppRoute.Favorites}
+        element={
+          <PrivateRoute isAuth>
+            <Favorites/>
+          </PrivateRoute>
         }
-        acc[cityName].push(offer);
-        return acc;
-      }, {});
-  }
-  return(
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path={AppRoute.Root}
-          element={<Main />}
-        />
-        <Route
-          path={AppRoute.Login}
-          element={<Login />}
-        />
-        <Route
-          path={AppRoute.Favorites}
-          element={
-            <PrivateRoute isAuth>
-              <Favorites offers={getFavoritesByCity(mockOffers)}/>
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path={`${AppRoute.Offer}:id`}
-          element={<Offer reviews={reviews}/>}
-        />
-        <Route
-          path='*'
-          element={<NotFound />}
-        />
-      </Routes>
-    </BrowserRouter>);
-
-};
+      />
+      <Route
+        path={`${AppRoute.Offer}:id`}
+        element={<Offer reviews={reviews}/>}
+      />
+      <Route
+        path='*'
+        element={<NotFound />}
+      />
+    </Routes>
+  </BrowserRouter>);
 
 export { App };
--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,55 +1,30 @@
 import { FavoritesList } from '../../components/FavoritesList/FavoritesList';
-import { FavoritesByCity } from '../../types/favorites';
+import { Header } from '../../components/Header/Header';
+import { useAppSelector } from '../../hooks/useAppSelector';
+import { getFavoritesByCity } from '../../utils/offers';
 
-type FavoritesProps ={
-  offers: FavoritesByCity;
-}
+const Favorites = () => {
+  const offers = useAppSelector((state)=> state.offers);
+  const favoritesOffers = getFavoritesByCity(offers);
+  return(
+    <div className="page">
+      <Header />
 
-const Favorites = ({offers} : FavoritesProps) => (
-  <div className="page">
-    <header className="header">
-      <div className="container">
-        <div className="header__wrapper">
-          <div className="header__left">
-            <a className="header__logo-link" href="main.html">
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-            </a>
-          </div>
-          <nav className="header__nav">
-            <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile" href="#">
-                  <div className="header__avatar-wrapper user__avatar-wrapper">
-                  </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  <span className="header__favorite-count">3</span>
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
+      <main className="page__main page__main--favorites">
+        <div className="page__favorites-container container">
+          <section className="favorites">
+            <h1 className="favorites__title">Saved listing</h1>
+            <FavoritesList offers={favoritesOffers}/>
+          </section>
         </div>
-      </div>
-    </header>
-
-    <main className="page__main page__main--favorites">
-      <div className="page__favorites-container container">
-        <section className="favorites">
-          <h1 className="favorites__title">Saved listing</h1>
-          <FavoritesList offers={offers}/>
-        </section>
-      </div>
-    </main>
-    <footer className="footer container">
-      <a className="footer__logo-link" href="main.html">
-        <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-      </a>
-    </footer>
-  </div>
-);
+      </main>
+      <footer className="footer container">
+        <a className="footer__logo-link" href="main.html">
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
+        </a>
+      </footer>
+    </div>
+  );
+};
 
 export { Favorites };
